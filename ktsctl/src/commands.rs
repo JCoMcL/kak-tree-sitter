40af11b993011fdf7683a0b23495a674468c0fc0
jcomcl <j@jcom.cl>
2026-05-08 15:58:37 +0100

    remove ktsctl

--- ktsctl/src/commands.rs
+++
@@ -1,5 +0,0 @@
-//! Main commands supported by ktsctl.
-
-pub mod manage;
-pub mod query;
-pub mod remove;
--- ktsctl/src/ui.rs
+++
@@ -1,9 +0,0 @@
-//! User Interface in the terminal.
-//!
-//! This module exports everything that is needed to provide feedback to the user.
-
-#[macro_use]
-pub mod report;
-pub mod section;
-pub mod source;
-pub mod table;
